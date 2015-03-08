c4889f61e849784c8e8547f0df34a698c560c981
arbind <arbind.thakur@gmail.com>
2015-03-07 21:14:18 -0800

    spec: fix bug where spy was being overloaded

--- spec/src/event-dispatcher-spec.js
+++ spec/src/event-dispatcher-spec.js
@@ -5,7 +5,6 @@ chai.use(spies);
 
 var expect = chai.expect;
 
-
 var EventDispatcher = require("event-dispatcher");
 
 describe("EventDispatcher", function() {
@@ -27,39 +26,49 @@ describe("EventDispatcher", function() {
     var responder = null;
 
     beforeEach(function() {
-      var handleEvent = function(payload) {eventPayload = payload;}
+      var handleEvent = function(payload) { eventPayload = payload;}
       responder = { 
         handleEvent: handleEvent,
         handleEvent2: handleEvent,
         handleEvent3: handleEvent
       };
-      handler = chai.spy(responder.handleEvent);
-      handler2 = chai.spy(responder.handleEvent2);
-      handler3 = chai.spy(responder.handleEvent3);
-      dispatcher.on(eventType, handler);
-      dispatcher.on(eventType, handler2);
-      dispatcher.on('ignored-event', handler3);
-      dispatcher.trigger(eventType, eventData);
     });
 
-    it("calls a handler registered for an event", function() {
-      expect(handler).to.have.been.called.once;
-    });
+    describe("given 1 handler registered for the given event", function(){
+      beforeEach(function() {
+        handler = chai.spy(responder.handleEvent);
+        dispatcher.on(eventType, handler);
+        dispatcher.trigger(eventType, eventData);
+      });
+      it("calls a handler registered for an event", function() {
+        expect(handler).to.have.been.called.once;
+      });
+      it("calls a handler registered for an event with the event payload", function() {
+        expect(handler).to.have.been.called.with(eventData);
+      });
+    })
+    describe("given multiple handler registered for the given event", function(){
+      beforeEach(function() {
+        handler = chai.spy(responder.handleEvent);
+        handler2 = chai.spy(responder.handleEvent2);
+        handler3 = chai.spy(responder.handleEvent3);
+        dispatcher.on(eventType, handler);
+        dispatcher.on(eventType, handler2);
+        dispatcher.on('ignored-event', handler3);
+        dispatcher.trigger(eventType, eventData);
+      });
 
-    it("calls a handler registered for an event with the event payload", function() {
-      expect(handler).to.have.been.called.with(eventData);
-    });
+      it("calls multiple handlers registered for an event", function() {
+        expect(handler).to.have.been.called.once;
+        expect(handler2).to.have.been.called.once;
+      });
 
-    it("calls multiple handlers registered for an event", function() {
-      expect(handler).to.have.been.called.once;
-      expect(handler2).to.have.been.called.once;
-    });
-
-    it("does not call handlers that registered for a different event", function() {
-      expect(handler).to.have.been.called();
-      expect(handler2).to.have.been.called();
-      expect(handler3).not.to.have.been.called();
-    });
+      it("does not call handlers that registered for a different event", function() {
+        expect(handler).to.have.been.called();
+        expect(handler2).to.have.been.called();
+        expect(handler3).not.to.have.been.called();
+      });
+    })
   });
   describe(".on", function() {
     var badRegisteration = null;
